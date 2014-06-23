7cbc20e506b17a22ae9ac9e6625ccf1ae416e2df
Darren Seet Serkai <darren@seet.me>
2014-06-23 09:31:42 +0900

    Completed chapter 19

---
+++ chapter19/exercise1.js
@@ -0,0 +1,18 @@
+/**
+ * Created by dseet on 6/23/2014.
+ */
+
+var friends = ['Meng', 'Leo', 'Howdy', 'Kiat', 'Henry'];
+alert(friends[2]);
+var name = friends[0];
+if(name == "Jennifer") {
+    alert("That's my name too!");
+} else {
+    alert("Sorry but my name is " + name);
+}
+var myVariable = 3;
+
+if(myVariable > 5)
+    alert('Upper');
+else
+    alert('Lower');
